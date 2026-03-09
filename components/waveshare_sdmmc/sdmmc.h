fc205161098c2c803569cf7c821d73ef33426d7a
sebalemes <75677210+sebalemes@users.noreply.github.com>
2026-03-09 19:55:46 -0300

    Update sdmmc.h

--- components/waveshare_sdmmc/sdmmc.h
+++ components/waveshare_sdmmc/sdmmc.h
@@ -1,10 +1,13 @@
 #pragma once
 
 #include "esphome/core/component.h"
+#include "esphome/core/automation.h"
+
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/text_sensor/text_sensor.h"
-#include "esphome/core/automation.h"
+
 #include "sdmmc_cmd.h"
+
 #include <string>
 
 namespace esphome {
@@ -15,18 +18,23 @@ class WaveshareSDMMC : public Component {
   void setup() override;
   void loop() override;
 
+  // Funções principais
   bool write_file(const char *path, const char *data);
   std::string read_file(const char *path);
 
+  // Sensores
   void set_total_space_sensor(sensor::Sensor *s) { this->total_space_sensor_ = s; }
   void set_free_space_sensor(sensor::Sensor *s) { this->free_space_sensor_ = s; }
   void set_last_error_sensor(text_sensor::TextSensor *s) { this->last_error_sensor_ = s; }
   void set_last_read_sensor(text_sensor::TextSensor *s) { this->last_read_sensor_ = s; }
 
+  // Atualização manual
   void update_sensors();
 
+  // Espaço do cartão
   size_t get_total_space();
   size_t get_free_space();
+
   bool is_initialized() const { return this->initialized_; }
 
   void set_last_error(const std::string &err) { this->last_error_ = err; }
@@ -38,12 +46,14 @@ class WaveshareSDMMC : public Component {
 
   sensor::Sensor *total_space_sensor_{nullptr};
   sensor::Sensor *free_space_sensor_{nullptr};
+
   text_sensor::TextSensor *last_error_sensor_{nullptr};
   text_sensor::TextSensor *last_read_sensor_{nullptr};
 
   std::string last_error_;
   std::string last_read_;
 };
+
 
 class UpdateSensorsAction : public Action<> {
  public:
