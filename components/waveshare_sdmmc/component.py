958b06a05c3ff094dc0af00b20e137fe226fc3f4
sebalemes <75677210+sebalemes@users.noreply.github.com>
2026-03-09 17:33:36 -0300

    Update component.py

--- components/waveshare_sdmmc/component.py
+++ components/waveshare_sdmmc/component.py
@@ -2,11 +2,11 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 
 waveshare_sdmmc_ns = cg.esphome_ns.namespace("waveshare_sdmmc")
-Waveshare_SDMMC = waveshare_sdmmc_ns.class_("WaveshareSDMMC", cg.Component)
+WaveshareSDMMC = waveshare_sdmmc_ns.class_("WaveshareSDMMC", cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(Waveshare_SDMMC),
-})
+    cv.GenerateID(): cv.declare_id(WaveshareSDMMC),
+}).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     var = cg.new_Pvariable(config[cv.GenerateID()])
