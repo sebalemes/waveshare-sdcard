d2514f9afeccf6ad1f9c793f315be52b72b8caae
sebalemes <75677210+sebalemes@users.noreply.github.com>
2026-03-09 18:52:10 -0300

    Update sdmmc.h

--- components/waveshare_sdmmc/sdmmc.h
+++ components/waveshare_sdmmc/sdmmc.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/text_sensor/text_sensor.h"
+#include "esphome/components/binary_sensor/binary_sensor.h"
 #include "sdmmc_cmd.h"
 
 namespace esphome {
@@ -11,12 +14,39 @@ class WaveshareSDMMC : public Component {
   void setup() override;
   void loop() override;
 
+  // Funções principais
   bool write_file(const char *path, const char *data);
   std::string read_file(const char *path);
+
+  // Sensores
+  void set_total_space_sensor(sensor::Sensor *s) { this->total_space_sensor_ = s; }
+  void set_free_space_sensor(sensor::Sensor *s) { this->free_space_sensor_ = s; }
+  void set_mounted_sensor(binary_sensor::BinarySensor *s) { this->mounted_sensor_ = s; }
+  void set_last_error_sensor(text_sensor::TextSensor *s) { this->last_error_sensor_ = s; }
+  void set_last_read_sensor(text_sensor::TextSensor *s) { this->last_read_sensor_ = s; }
+
+  // Funções auxiliares
+  size_t get_total_space();
+  size_t get_free_space();
+  bool is_initialized() const { return this->initialized_; }
+
+  void set_last_error(const std::string &err) { this->last_error_ = err; }
+  void set_last_read(const std::string &txt) { this->last_read_ = txt; }
 
  protected:
   sdmmc_card_t *card_{nullptr};
   bool initialized_{false};
+
+  // Sensores
+  sensor::Sensor *total_space_sensor_{nullptr};
+  sensor::Sensor *free_space_sensor_{nullptr};
+  binary_sensor::BinarySensor *mounted_sensor_{nullptr};
+  text_sensor::TextSensor *last_error_sensor_{nullptr};
+  text_sensor::TextSensor *last_read_sensor_{nullptr};
+
+  // Armazenamento interno
+  std::string last_error_;
+  std::string last_read_;
 };
 
 }  // namespace waveshare_sdmmc
