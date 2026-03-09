e02412e0d10c85577506f810ec7df4e86234972f
sebalemes <75677210+sebalemes@users.noreply.github.com>
2026-03-09 18:56:59 -0300

    Update __init__.py

--- components/waveshare_sdmmc/__init__.py
+++ components/waveshare_sdmmc/__init__.py
@@ -1,23 +1,50 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
 from esphome import automation
+from esphome.components import sensor, text_sensor, binary_sensor
 
-WRITE_ACTION_SCHEMA = cv.Schema({
-    cv.Required("path"): cv.string,
-    cv.Required("data"): cv.string,
-})
+waveshare_sdmmc_ns = cg.esphome_ns.namespace("waveshare_sdmmc")
+WaveshareSDMMC = waveshare_sdmmc_ns.class_("WaveshareSDMMC", cg.Component)
 
-READ_ACTION_SCHEMA = cv.Schema({
-    cv.Required("path"): cv.string,
-})
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(WaveshareSDMMC),
 
-@automation.register_action("waveshare_sdmmc.write_file", WRITE_ACTION_SCHEMA)
-async def write_file_action(config, action_id, template_arg):
+    cv.Optional("total_space"): sensor.sensor_schema(),
+    cv.Optional("free_space"): sensor.sensor_schema(),
+    cv.Optional("mounted"): binary_sensor.binary_sensor_schema(),
+    cv.Optional("last_error"): text_sensor.text_sensor_schema(),
+    cv.Optional("last_read"): text_sensor.text_sensor_schema(),
+
+}).extend(cv.COMPONENT_SCHEMA)
+
+# Ação para atualizar sensores manualmente
+UPDATE_ACTION_SCHEMA = cv.Schema({})
+
+@automation.register_action("waveshare_sdmmc.update_sensors", UPDATE_ACTION_SCHEMA)
+async def update_sensors_action(config, action_id, template_arg):
     var = cg.new_Pvariable(action_id, template_arg)
-    cg.add(var.set_path(config["path"]))
-    cg.add(var.set_data(config["data"]))
     return var
 
-@automation.register_action("waveshare_sdmmc.read_file", READ_ACTION_SCHEMA)
-async def read_file_action(config, action_id, template_arg):
-    var = cg.new_Pvariable(action_id, template_arg)
-    cg.add(var.set_path(config["path"]))
-    return var
+async def to_code(config):
+    var = cg.new_Pvariable(config[cv.GenerateID()])
+    await cg.register_component(var, config)
+
+    if "total_space" in config:
+        sens = await sensor.new_sensor(config["total_space"])
+        cg.add(var.set_total_space_sensor(sens))
+
+    if "free_space" in config:
+        sens = await sensor.new_sensor(config["free_space"])
+        cg.add(var.set_free_space_sensor(sens))
+
+    if "mounted" in config:
+        sens = await binary_sensor.new_binary_sensor(config["mounted"])
+        cg.add(var.set_mounted_sensor(sens))
+
+    if "last_error" in config:
+        sens = await text_sensor.new_text_sensor(config["last_error"])
+        cg.add(var.set_last_error_sensor(sens))
+
+    if "last_read" in config:
+        sens = await text_sensor.new_text_sensor(config["last_read"])
+        cg.add(var.set_last_read_sensor(sens))
