5135e44309e010cc37cf78a29d08390cc6b71f0b
sebalemes <75677210+sebalemes@users.noreply.github.com>
2026-03-09 19:33:56 -0300

    Update sdmmc.h

--- components/waveshare_sdmmc/sdmmc.h
+++ components/waveshare_sdmmc/sdmmc.h
@@ -3,7 +3,9 @@
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/text_sensor/text_sensor.h"
+#include "esphome/components/binary_sensor/binary_sensor.h"
 #include "sdmmc_cmd.h"
+#include <string>
 
 namespace esphome {
 namespace waveshare_sdmmc {
@@ -20,6 +22,7 @@ class WaveshareSDMMC : public Component {
   // Sensores
   void set_total_space_sensor(sensor::Sensor *s) { this->total_space_sensor_ = s; }
   void set_free_space_sensor(sensor::Sensor *s) { this->free_space_sensor_ = s; }
+  void set_mounted_sensor(binary_sensor::BinarySensor *s) { this->mounted_sensor_ = s; }
   void set_last_error_sensor(text_sensor::TextSensor *s) { this->last_error_sensor_ = s; }
   void set_last_read_sensor(text_sensor::TextSensor *s) { this->last_read_sensor_ = s; }
 
@@ -49,12 +52,17 @@ class WaveshareSDMMC : public Component {
   std::string last_error_;
   std::string last_read_;
 };
+
 class UpdateSensorsAction : public Action<> {
  public:
-  UpdateSensorsAction(WaveshareSDMMC *parent) : parent_(parent) {}
+  explicit UpdateSensorsAction(WaveshareSDMMC *parent) : parent_(parent) {}
+
   void play() override {
-    this->parent_->update_sensors();
+    if (this->parent_ != nullptr) {
+      this->parent_->update_sensors();
+    }
   }
+
  protected:
   WaveshareSDMMC *parent_;
 };
