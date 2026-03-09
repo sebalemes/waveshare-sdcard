4e58917ee4d0b298b326819effe5c67bc23830c7
sebalemes <75677210+sebalemes@users.noreply.github.com>
2026-03-09 16:59:47 -0300

    Update component.py

--- components/sdmmc_s3/component.py
+++ components/sdmmc_s3/component.py
@@ -2,13 +2,13 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import component
 
-sdmmc_ns = cg.esphome_ns.namespace("sdmmc_s3")
-SDMMCS3 = sdmmc_ns.class_("SDMMCS3", component.Component)
+sdmmc_s3_ns = cg.esphome_ns.namespace("sdmmc_s3")
+SDMMCS3 = sdmmc_s3_ns.class_("SDMMCS3", cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(SDMMCS3),
 })
 
 async def to_code(config):
-    var = cg.new_Pvariable(config["id"])
-    await component.register_component(var, config)
+    var = cg.new_Pvariable(config[cv.GenerateID()])
+    await cg.register_component(var, config)
